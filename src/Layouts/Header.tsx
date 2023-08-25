754f3009615f647e3025e5598818f0a8fada61d1
ravenishka <valeriya.voloshenyuk@gmail.com>
2023-08-25 22:58:03 +0300

    удалила меню в header

--- src/Layouts/Header.tsx
+++ src/Layouts/Header.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import stylesheet from '../Assets/Styles/Header.module.scss';
-import { Layout, Menu } from 'antd';
+import { Layout } from 'antd';
 import { AntDesignOutlined, LogoutOutlined } from '@ant-design/icons';
 import { Avatar } from 'antd';
 const { Header } = Layout;
 
-const items1 = ['1', '2', '3'].map((key) => ({
-    key,
-    label: `nav ${key}`,
-}));
+
 
 const HeaderApp: React.FC = () => {
     return (
@@ -18,7 +15,7 @@ const HeaderApp: React.FC = () => {
             size={60}
           icon={<AntDesignOutlined />} // лого
         />
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} items={items1} />
+        
         </div>
 
         <Avatar
