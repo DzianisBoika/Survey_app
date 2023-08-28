baf3e9c69072b36bec1f54568d34e607684a921a
ravenishka <valeriya.voloshenyuk@gmail.com>
2023-08-28 11:39:43 +0300

    изменила лого

--- src/Layouts/Header.tsx
+++ src/Layouts/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import stylesheet from '../Assets/Styles/Header.module.scss';
 import { Layout } from 'antd';
 import { AntDesignOutlined, LogoutOutlined } from '@ant-design/icons';
+import Logo from '../Assets/img/logo.svg';
 import { Avatar } from 'antd';
 const { Header } = Layout;
 
@@ -9,18 +10,15 @@ const { Header } = Layout;
 
 const HeaderApp: React.FC = () => {
     return (
-    <Header className={stylesheet.Header}>
-        <div className={stylesheet.container}>
-        <Avatar
-            size={60}
-          icon={<AntDesignOutlined />} // лого
-        />
-        
+        <Header className={stylesheet.Header}>
+            <div className={stylesheet.container}>
+                
+            <img src={Logo} alt="Logo"  height={40} />
         </div>
 
         <Avatar
-        size={60}
-        icon={<LogoutOutlined />} // кнопка выхода
+            size={60}
+            icon={<LogoutOutlined />} // кнопка выхода
         />
     </Header>
     );
