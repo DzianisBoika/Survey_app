cc34a83c334e5e3ebd8cdeb1fe139f1f860b77d6
ravenishka <valeriya.voloshenyuk@gmail.com>
2023-09-20 11:35:44 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,16 @@ import HeaderApp from './Layouts/Header.tsx';
 import ContentApp from './Layouts/Content.tsx';
 import SiderApp from './Layouts/Sider.tsx';
 import stylesheet from './Assets/Styles/App.module.scss';
-import FooterApp from './Layouts/Footer.tsx';
 
 
 const App: React.FC = () => {
   return (
     <>
-      
-        
-        <HeaderApp />
-        <div className={stylesheet.siderContent}>
-          <SiderApp />
-          <ContentApp  />
-        </div>
-          
-      
+      <HeaderApp />
+      <div className={stylesheet.siderContent}>
+        <SiderApp />
+        <ContentApp  />
+      </div>
     </>
   );
 };
--- src/Layouts/Sider.tsx
+++ src/Layouts/Sider.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import stylesheet from '../Assets/Styles/Sider.module.scss'
 import { LaptopOutlined } from '@ant-design/icons';
-import { Button, Layout, Menu } from 'antd';
+import { Layout, Menu } from 'antd';
 const { Sider } = Layout;
 
 
@@ -13,33 +13,30 @@ const items2 = [LaptopOutlined].map(
             icon: React.createElement(icon),
             label: `Ваши опросы`,
 
-        children: new Array(4).fill(null).map((_, j) => {
-        const subKey = index * 4 + j + 1;
-            return {
-                key: subKey,
-                label: `Таблица опроса ${subKey}`,
+            children: new Array(4).fill(null).map((_, j) => {
+            const subKey = index * 4 + j + 1;
+                return {
+                    key: subKey,
+                    label: `Таблица опроса ${subKey}`,
+                };
+            }),
         };
-        }),
-    };
     }, 
     
 );
 
 const SiderApp: React.FC = () => {
     return (
-        
-            <div className={stylesheet.container}>
-                <Sider >
-                    <Menu className={stylesheet.menu}
-                        mode="inline"
-                        defaultSelectedKeys={['1']}
-                        defaultOpenKeys={['sub1']}
-                        
-                        items={items2}                    
-                    />
-                </Sider>
-            </div>
-        
+        <div className={stylesheet.container}>
+            <Sider >
+                <Menu className={stylesheet.menu}
+                    mode="inline"
+                    defaultSelectedKeys={['1']}
+                    defaultOpenKeys={['sub1']}
+                    items={items2}                    
+                />
+            </Sider>
+        </div>
     );
 };
 
